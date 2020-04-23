3c8022bf982aa9f801c1dc92964e10f5d901502c
dayuss <ddtardian@gmail.com>
2020-04-23 12:10:57 +0700

    Initial commit

---
+++ application/controllers/Landing.php
@@ -0,0 +1,11 @@
+<?php 	
+
+class Landing extends CI_Controller
+{
+	public function index(){
+		// dd($this->bcrypt->hash_password('admin1'));
+        $this->load->view('landing/index',array('title'=>'PPDB ONLINE'));
+	}
+
+}
+ ?>
